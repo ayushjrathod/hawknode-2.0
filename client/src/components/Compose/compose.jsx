d677125f318635638a06eee0c411e364016e6332
Ayush Rathod <clashofa1057@gmail.com>
2024-06-27 15:05:18 +0530

    chore: Add react-quill

--- client/src/components/Compose/compose.jsx
+++ client/src/components/Compose/compose.jsx
@@ -1,73 +1,53 @@
-import { Editor } from "@tinymce/tinymce-react";
-import axios from "../../api/axios.jsx";
-import {useState,useRef,useCallback} from "react";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
+import Preview from "./preview";
 
-
-function Compose() {
-  const titleRef =useRef();
+const Compose = () => {
+  const [description, setDescription] = useState("");
+  const [ publish, setPublish ] = useState(false);
   const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  
-
-  const handleEditorChange = useCallback((content, editor) => {
-    setContent(content);
-    setTitle(titleRef.current.value);
-  }, []);
-
-  const data={
-    title:title,
-    content:content,
-  }
-
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    console.log(data);
-    axios
-    .post("/v1/posts/create-post",data)
-    .then((response)=>{
-      console.log(response);
-    })
-    .catch((error)=>{
-      console.log(error);
-    });
-
-
-  };
 
   return (
-    <>
-      {/* <h1 className="font-Akshar text-4xl font-medium px-10 py-3">Compose</h1> */}
-      <div className="flex justify-between m-2">
-      <input ref={titleRef} placeholder="Title" className="w-[80%] p-2 top-[116px] rounded-md font-Akshar text-2xl outline-none"/>
-
-      <button
-        onClick={handleClick}
-        className="bg-black text-white font-Akshar font-bold p-2 rounded-3xl px-8 mx-6 text-xl"
-      >
-        Post
-      </button>
+    <div>
+      <div>
+        <button
+          onClick={() => setPublish(true)}
+          className="bg-green-700 p-2 font-Akshar text-lg font-semibold text-white rounded-full absolute right-0 m-2"
+        >
+          Publish
+        </button>
       </div>
+      <section className="w-[90%] md:w-[90%] lg:w-[60%] mx-auto py-[3rem]">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Title"
+          className="text-4xl outline-none w-full"
+        />
 
-      <Editor
-        apiKey="8w3gs7kyl0a3tx4ajvykqc6cv9u592zxc9el6ifsdvqsqezd"
-        initialValue=""
-        onEditorChange={handleEditorChange}
-        init={{
-          inline_boundaries_selector:false,
-          inline_boundaries:false,  
-          height:500,
-          placeholder:"I am waiting for your thougts ...",
-          menubar:false,
-          plugins:
-            "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
-          toolbar:
-            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough code | link image media table | align lineheight | checklist numlist bullist indent outdent",
-        }}
-      />
-
-    </>
+        <ReactQuill
+          theme="bubble"
+          value={description}
+          onChange={setDescription}
+          placeholder="Waiting for your thoughts ..."
+          className="write my-5"
+        />
+        <div
+          className={`${
+            publish ? "visible opacity-100" : "invisible opacity-0"
+          } transition-all duration-200`}
+        >
+          <Preview
+            setPublish={setPublish}
+            description={description}
+            title={title}
+          />
+        </div>
+      </section>
+    </div>
   );
-}
+};
 
 export default Compose;
